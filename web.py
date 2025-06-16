12c2909c41a911d09616906294b145ede6470912
RushoBinnabi <69098829+RushoBinnabi@users.noreply.github.com>
2025-06-15 23:45:42 -0400

    Add files via upload

--- web.py
+++ web.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time
 
 driver = ""
 videoLinkElement = ""
+newVideoLinkElement = ""
 
 def checkVideos(browser, videoOldSiteLink, videoNewYouTubeLink):
     global driver
@@ -12,8 +14,13 @@ def checkVideos(browser, videoOldSiteLink, videoNewYouTubeLink):
         driver = webdriver.Firefox()
     driver.get(videoOldSiteLink)
     videoLinkElement = driver.find_element(By.CLASS_NAME, "site-body-center-column").get_attribute("href")
+    driver.execute_script("window.open("");")
+    driver.switch_to.window(driver.window_handles[1])
+    driver.get(videoNewYouTubeLink)
+    time.sleep(30)
+    newVideoLinkElement = driver.find_element(By.CLASS_NAME, "style-scope ytd-player").get_attribute("href")
     print()
-    print("Is the new video on the site? => " + str(videoLinkElement == videoNewYouTubeLink))
+    print("Is the new video on the site? => " + str(videoLinkElement == newVideoLinkElement))
     driver.quit()
     print()
     input("Press any key to continue...")
