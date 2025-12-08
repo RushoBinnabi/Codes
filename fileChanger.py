e1ab0bc77100745498dce1133b39c80dc0355b91
RushoBinnabi <69098829+RushoBinnabi@users.noreply.github.com>
2025-12-08 10:29:33 -0500

    Add files via upload

--- fileChanger.py
+++ fileChanger.py
@@ -9,14 +9,26 @@
 
 import os
 
-def changeFileExtension(oldFileExtension, newFileExtension):
-    for file in os.listdir():
-        if file.endswith(oldFileExtension):
-            oldFileName = os.path.splitext(file)[0]
-            oldFileExtension = os.path.splitext(file)[1]
-            os.rename(oldFileName + oldFileExtension, oldFileName + newFileExtension)
+def changeFileExtension(directory, oldExtension, newExtension):
+    for file in os.listdir(directory):
+        if file.endswith(oldExtension):
+            obj = os.path.splitext(file)[0]
+            ext = os.path.splitext(file)[1]
+            os.rename(obj + ext, obj + newExtension)
 
+folder = input("Enter Directory: ")
 oldFileExtension = input("Enter Old File Extension: ")
 newFileExtension = input("Enter New File Extension: ")
 
-changeFileExtension(oldFileExtension, newFileExtension)
+changeFileExtension(folder, oldFileExtension, newFileExtension)
+
+condition = input("\nDo you want to use the program again? (y/n) ")
+
+while condition == "y":
+    folder = input("Enter Directory: ")
+    oldFileExtension = input("Enter Old File Extension: ")
+    newFileExtension = input("Enter New File Extension: ")
+    changeFileExtension(folder, oldFileExtension, newFileExtension)
+    condition = input("\nDo you want to use the program again? (y/n) ")
+    if condition == "n":
+        exit
